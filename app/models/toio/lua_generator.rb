34d5d65c9ab05018c1bb9b37a3a5c9f13e579223
teach310 <okms310@gmail.com>
2023-04-19 15:26:03 +0900

    cubeを動かす

--- app/models/toio/lua_generator.rb
+++ app/models/toio/lua_generator.rb
@@ -4,11 +4,7 @@ module Toio
   class LuaGenerator
     def generate
       <<~LUA
-        local toio = require("toio")
-        local cube = toio.Simulator.new()
-        cube:connect()
-        cube:move(100, 100, 1000)
-        cube:move(0, 0, 1000)
+        cubeCommand:Move('cube1', 50, -50, 100)
       LUA
     end
   end
