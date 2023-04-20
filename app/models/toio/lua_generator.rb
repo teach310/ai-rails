e5a1e735cfe949a36cb488968c19d315518d19ca
teach310 <okms310@gmail.com>
2023-04-20 14:30:32 +0900

    コードブロックからluaを抽出する機能を作成

--- app/models/toio/lua_generator.rb
+++ app/models/toio/lua_generator.rb
@@ -4,14 +4,48 @@ module Toio
   class LuaGenerator
     def generate(src_messages = [])
       response = post_chatgpt(src_messages)
-      response.dig("choices", 0, "message", "content")
+      chatgpt_content = response.dig("choices", 0, "message", "content")
+      extract_lua(chatgpt_content)
     end
 
     def build_messages(src_messages)
       [{ role: 'system', content: system_content }] + src_messages
     end
 
+    def extract_lua(chatgpt_content)
+      # 最初の文字がfunctionの時はそのまま返す
+      return chatgpt_content if chatgpt_content.start_with?('function')
+      
+      # 「```」という文字列を含む場合には
+      if chatgpt_content.include?('```')
+        extracted_lua = extract_lua_from_code_block(chatgpt_content)
+        if extracted_lua.present?
+          return extracted_lua
+        else
+          return generate_failed('extract code block failed') # 「'」が文章に入るのを避けるために固定文言返す
+        end
+      end
+      
+      generate_failed(chatgpt_content)
+    end
+
     private
+
+    def extract_lua_from_code_block(chatgpt_content)
+      regex = /^```.*?\r?\n((.*?\r?\n)*?)```/m;
+      m = regex.match(chatgpt_content)
+      return m[1] if m
+      nil
+    end
+
+    def generate_failed(failed_content)
+      <<~LUA
+        function routine()
+          cubeCommand:ShowMessage('generate failed')
+          cubeCommand:ShowMessage(\'#{failed_content}\')
+        end
+      LUA
+    end
 
     def post_chatgpt(src_messages)
       client.chat(
@@ -32,6 +66,12 @@ module Toio
           {write command here}
         end
 
+        ### Coordinate
+        origin is left bottom.
+
+        x: left 45 ~ right 455
+        y: bottom 45 ~ top 455
+
         ## Command List
 
         cubeCommand:ShowMessage(content)
@@ -44,15 +84,32 @@ module Toio
         // seconds | duration seconds | range (0.1~30.0)
         coroutine.yield(CS.UnityEngine.WaitForSeconds(seconds))
 
-        ## Example
+        // Move to target world position
+        coroutine.yield(cubeCommand:Navi2TargetCoroutine('cube1', x, y))
+
+        // Look to target rotation
+        // deg: 0~359
+        // example) look to right: 0, look to top: -90
+        coroutine.yield(cubeCommand:Rotate2DegCoroutine('cube1', deg))
+        
+        ## Example1 move by motor speed
 
         function routine()
-          cubeCommand:ShowMessage('動きます!')
+          cubeCommand:ShowMessage('start!')
           cubeCommand:Move('cube1', 50, -50, 100)
           coroutine.yield(CS.UnityEngine.WaitForSeconds(1))
           cubeCommand:Move('cube1', 50, -50, 100)
           coroutine.yield(CS.UnityEngine.WaitForSeconds(1))
-          cubeCommand:ShowMessage('終了！')
+          cubeCommand:ShowMessage('end!')
+        end
+
+        ## Example2 set start position
+
+        function routine()
+          cubeCommand:ShowMessage('Go to start position (=center) and look forward')
+          coroutine.yield(cubeCommand:Navi2TargetCoroutine('cube1', 250, 250))
+          coroutine.yield(cubeCommand:Rotate2DegCoroutine('cube1', -90))
+          cubeCommand:ShowMessage('Ready!')
         end
       CONTENT
     end
--- spec/models/toio/lua_generator_spec.rb
+++ spec/models/toio/lua_generator_spec.rb
@@ -15,4 +15,85 @@ RSpec.describe Toio::LuaGenerator, type: :model do
       expect(got.size).to eq 2
     end
   end
+
+  describe '#extract_lua' do
+    subject { generator.extract_lua(chatgpt_content) }
+
+    context '最初の文字がfunctionの時' do
+      let(:chatgpt_content) do
+        <<~CONTENT
+          function routine()
+            cubeCommand:ShowMessage('Go to start position (=center) and look forward')
+            coroutine.yield(cubeCommand:Navi2TargetCoroutine('cube1', 250, 250))
+            coroutine.yield(cubeCommand:Rotate2DegCoroutine('cube1', -90))
+            cubeCommand:ShowMessage('Ready!')
+          end
+        CONTENT
+      end
+
+      it { is_expected.to eq chatgpt_content }
+    end
+
+    context '「```」という文字列で囲まれている場合' do
+      let(:chatgpt_content) do
+        <<~CONTENT
+          To look to the top, you can use the `Rotate2DegCoroutine` command with a degree value of -90. Here's an example:
+
+          ```
+          function routine()
+            cubeCommand:ShowMessage('Looking to the top')
+            coroutine.yield(cubeCommand:Rotate2DegCoroutine('cube1', -90))
+            cubeCommand:ShowMessage('Done')
+          end
+          ```
+        CONTENT
+      end
+
+      it 'コードブロックを抽出して返す' do
+        is_expected.to eq <<~CONTENT
+          function routine()
+            cubeCommand:ShowMessage('Looking to the top')
+            coroutine.yield(cubeCommand:Rotate2DegCoroutine('cube1', -90))
+            cubeCommand:ShowMessage('Done')
+          end
+        CONTENT
+      end
+    end
+
+    context '「```」という文字列があって囲まれていない場合' do
+      let(:chatgpt_content) do
+        <<~CONTENT
+          To look to the top, you can use the `Rotate2DegCoroutine` command with a degree value of -90. Here's an example:
+
+          ```
+          function routine()
+            cubeCommand:ShowMessage('Looking to the top')
+            coroutine.yield(cubeCommand:Rotate2DegCoroutine('cube1', -90))
+            cubeCommand:ShowMessage('Done')
+        CONTENT
+      end
+
+      it '失敗' do
+        is_expected.to eq <<~CONTENT
+          function routine()
+            cubeCommand:ShowMessage('generate failed')
+            cubeCommand:ShowMessage('extract code block failed')
+          end
+        CONTENT
+      end
+    end
+
+    context 'それ以外の場合' do
+      let(:chatgpt_content) { 'hoge' }
+
+      it '失敗' do
+        is_expected.to eq <<~CONTENT
+          function routine()
+            cubeCommand:ShowMessage('generate failed')
+            cubeCommand:ShowMessage('hoge')
+          end
+        CONTENT
+      end
+    end
+  end
 end
